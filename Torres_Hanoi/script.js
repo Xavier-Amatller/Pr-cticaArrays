56dcd3e86e61cb01bb6e3512bf067c813ad06dce
Xavier-Amatller <reyo2004@gmail.com>
2024-12-05 18:34:08 +0100

    Torres de Hanoi acabada

--- Torres_Hanoi/script.js
+++ Torres_Hanoi/script.js
@@ -1,29 +1,30 @@
-let towers = {
-    tower1: [5, 4, 3, 2, 1],
-    tower2: [],
-    tower3: []
-};
+let towers = [
+    [],
+    [],
+    []
+];
 
+document.getElementById("submit").addEventListener("click", () => {
+    let nPiezas = document.getElementById("nPiezas").value;
+    for (let index = 1; index <= nPiezas; index++) {
+        towers[0].splice(0, 0, index);
+    }
+    document.getElementById("nPiezas-div").innerHTML = "";
+    renderTowers()
+    console.log(towers);
+});
 
 function moveFromTo(from, to) {
 
-    fromTower = towers["tower" + from];
-    toTower = towers["tower" + to];
+    fromTower = towers[from - 1];
+    toTower = towers[to - 1];
 
     if (fromTower.length == 0) {
         alert("No puedes mover algo que no existe crack");
-    } else if (fromTower[fromTower.length - 1] > toTower[toTower - 1]) {
+    } else if (fromTower[fromTower.length - 1] > toTower[toTower.length - 1]) {
         alert("no puedes meter eso ahí");
     } else {
-
-        //MOVE PIECE
         toTower.push(fromTower.pop());
-
-        console.log(towers["tower1"]);
-        console.log(towers["tower2"]);
-        console.log(towers["tower3"]);
-
-        //RENDER TOWERS
         renderTowers();
     }
 }
@@ -34,10 +35,16 @@ function renderTowers() {
     let towe2div = document.getElementById("tower2-div").innerHTML = "";
     let towe3div = document.getElementById("tower3-div").innerHTML = "";
 
-    towers["tower1"].forEach(element => {
-        let piece = '<div class = piece-"' + element + '"></div>';
-        document.getElementById("tower1-div").innerHTML = piece;
-    });
+    for (let index = 0; index < 3; index++) {
+
+        let auxTower = towers[index].toReversed();
+
+        auxTower.forEach(element => {
+            let piece = '<div class = piece-' + element + '></div>';
+            let tower = 'tower' + (index + 1) + '-div';
+            document.getElementById(tower).innerHTML += piece;
+        });
 
 
+    }
 }
